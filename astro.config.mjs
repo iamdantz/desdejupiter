f6c701da06489b6e068fa9783ef638eb399acbeb
Dantz <dantz@Jupiter.>
2025-03-14 13:48:53 -0600

    Update title size

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ export default defineConfig({
   site: 'https://desdejupiter.me',
   vite: {
     plugins: [tailwindcss(), eslintPlugin()],
+    server: {
+      allowedHosts: ['215e-2806-109f-10-6cd2-e83b-c5a3-f7be-266c.ngrok-free.app'],
+    },
   },
   integrations: [icon()],
 });
