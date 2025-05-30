3bf4401b8f6a7c202137d73b83171c642d92cbcb
Dantz <37550167+iamdantz@users.noreply.github.com>
2025-05-30 13:04:30 -0600

    update consts

--- src/consts.ts
+++ src/consts.ts
@@ -5,20 +5,20 @@ export const SITE_TITLE = 'Desde Júpiter';
 export const SITE_DESCRIPTION = 'Me dedico a crear soluciones simples a problemas complejos. Comparto conocimientos sobre arquitectura cloud, diseño de software y liderazgo.';
 
 export const MENU = [
-    { title: 'Inicio', url: '/', icon: 'hugeicons:home-03', active: true },
-    { title: 'Blog', url: '/blog', icon: 'hugeicons:quill-write-02', active: true },
-    { title: 'Perfil', url: '/about', icon: 'hugeicons:quill-write-02', active: true },
-    { title: 'Contacto', url: 'https://www.linkedin.com/in/heydantz/', icon: 'hugeicons:mail-02', active: true },
+  { title: 'Inicio', url: '/', icon: 'hugeicons:home-03', active: true },
+  { title: 'Blog', url: 'https://blog.desdejupiter.me', icon: 'hugeicons:quill-write-02', active: true },
+  { title: 'Perfil', url: '/about', icon: 'hugeicons:quill-write-02', active: true },
+  { title: 'Contacto', url: 'https://www.linkedin.com/in/heydantz/', icon: 'hugeicons:mail-02', active: true },
 ];
 
 export const SOCIALS = [
-    {
-      platform: 'LinkedIn',
-      url: 'https://www.linkedin.com/in/heydantz/',
-      icon: 'hugeicons:linkedin-01',
-    },
-    { platform: 'GitHub', url: 'https://github.com/heydantz', icon: 'hugeicons:github-01' },
-  ];
+  {
+    platform: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/heydantz/',
+    icon: 'hugeicons:linkedin-01',
+  },
+  { platform: 'GitHub', url: 'https://github.com/heydantz', icon: 'hugeicons:github-01' },
+];
 
 export const POSTS_PER_PAGE = 9;
 export const BLOG_URL = '/blog';
