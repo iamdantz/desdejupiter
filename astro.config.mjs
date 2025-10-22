2ad1a64dcc81403e9c20beb38c91350918c90ec0
Dantz <37550167+iamdantz@users.noreply.github.com>
2025-10-22 12:02:42 -0600

    upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,44 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import robots from "astro-robots";
+
+import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel";
+
 export default defineConfig({
+  site: "https://desdejupiter.me",
+  output: 'server',
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [
+    sitemap(),
+    robots({
+      host: "desdejupiter.me",
+      sitemap: [
+        "https://desdejupiter.me/sitemap-index.xml",
+        "https://www.desdejupiter.me/sitemap-index.xml",
+      ],
+      policy: [
+        {
+          userAgent: [
+            "Googlebot",
+            "bingbot",
+            "Applebot",
+            "Yandex",
+            "DuckDuckBot",
+          ],
+          allow: ["/"],
+          crawlDelay: 0,
+        },
+        {
+          userAgent: "AhrefsBot",
+          disallow: ["/"],
+        },
+      ],
+    }),
+  ],
 });
